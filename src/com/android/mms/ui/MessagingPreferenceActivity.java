5d7cbe18c0c8d711f8e216c2af24ac2fa3a28d0e
cphelps76 <cphelps76@gmail.com>
2013-06-17 11:58:25 -0500

    Revert "Breathing SMS Notification"
    This reverts commit 845de7b70e42dc79911a5f037bf82303298a3477.
    
    Conflicts:
    	res/drawable-xhdpi/stat_notify_sms_0.png
    	res/drawable-xhdpi/stat_notify_sms_1.png
    	res/drawable-xhdpi/stat_notify_sms_10.png
    	res/drawable-xhdpi/stat_notify_sms_11.png
    	res/drawable-xhdpi/stat_notify_sms_12.png
    	res/drawable-xhdpi/stat_notify_sms_13.png
    	res/drawable-xhdpi/stat_notify_sms_14.png
    	res/drawable-xhdpi/stat_notify_sms_15.png
    	res/drawable-xhdpi/stat_notify_sms_16.png
    	res/drawable-xhdpi/stat_notify_sms_17.png
    	res/drawable-xhdpi/stat_notify_sms_18.png
    	res/drawable-xhdpi/stat_notify_sms_19.png
    	res/drawable-xhdpi/stat_notify_sms_2.png
    	res/drawable-xhdpi/stat_notify_sms_20.png
    	res/drawable-xhdpi/stat_notify_sms_3.png
    	res/drawable-xhdpi/stat_notify_sms_4.png
    	res/drawable-xhdpi/stat_notify_sms_5.png
    	res/drawable-xhdpi/stat_notify_sms_6.png
    	res/drawable-xhdpi/stat_notify_sms_7.png
    	res/drawable-xhdpi/stat_notify_sms_8.png
    	res/drawable-xhdpi/stat_notify_sms_9.png
    	res/values/strings.xml

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -66,7 +66,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String NOTIFICATION_VIBRATE     = "pref_key_vibrate";
     public static final String NOTIFICATION_VIBRATE_WHEN= "pref_key_vibrateWhen";
     public static final String NOTIFICATION_RINGTONE    = "pref_key_ringtone";
-    public static final String NOTIFICATION_BREATH      = "pref_key_sms_breath";
     public static final String AUTO_RETRIEVAL           = "pref_key_mms_auto_retrieval";
     public static final String RETRIEVAL_DURING_ROAMING = "pref_key_mms_retrieval_during_roaming";
     public static final String AUTO_DELETE              = "pref_key_auto_delete";
@@ -132,7 +131,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     private Preference mManageSimPref;
     private Preference mClearHistoryPref;
     private ListPreference mVibrateWhenPref;
-    private CheckBoxPreference mBreathPref;
     private CheckBoxPreference mEnableNotificationsPref;
     private CheckBoxPreference mEnablePrivacyModePref;
     private CheckBoxPreference mMmsAutoRetrievialPref;
@@ -230,9 +228,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
         // Vibration
         mVibrateEntries = getResources().getTextArray(R.array.prefEntries_vibrateWhen);
         mVibrateValues = getResources().getTextArray(R.array.prefValues_vibrateWhen);
-
-        // Statbar notification breath
-        mBreathPref = (CheckBoxPreference) findPreference(NOTIFICATION_BREATH);
 
         setMessagePreferences();
     }
@@ -635,10 +630,6 @@ public class MessagingPreferenceActivity extends PreferenceActivity
             PreferenceManager.getDefaultSharedPreferences(context).edit();
         editor.putBoolean(MessagingPreferenceActivity.QM_LOCKSCREEN_ENABLED, enabled);
         editor.apply();
-
-    public static boolean getBreathEnabled(Context context) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        return prefs.getBoolean(MessagingPreferenceActivity.NOTIFICATION_BREATH, false);
     }
 
     public static boolean getQmCloseAllEnabled(Context context) {
